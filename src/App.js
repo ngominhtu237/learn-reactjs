81503cc1085791c4572019f2bed12661d31af0f0
tu.nm1 <ngominhtu237@gmail.com>
2021-02-17 21:20:14 +0700

    state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <p>Hello world!</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">My hobbies: Racing</Person>
-        <Person name="Stephanie" age="26" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
