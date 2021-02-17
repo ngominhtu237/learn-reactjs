63acbc50af0816c691a92d8a669b99481b46eb7a
tu.nm1 <ngominhtu237@gmail.com>
2021-02-18 00:01:43 +0700

    two-way binding

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ class App extends Component {
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -33,7 +43,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}>
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangeHandler}>
             My hobbies: Racing
         </Person>
 
