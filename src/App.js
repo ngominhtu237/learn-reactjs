1aa7df0a3bd202fd2e335422b118ad82b40b1496
tu.nm1 <ngominhtu237@gmail.com>
2021-02-19 13:00:19 +0700

    m1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-    background-color: ${props => props.alt ? 'red' : 'green'};
-    color: white;
-    font: inherit;
-    border: 1px solid blue;
-    padding: 8px;
-    cursor: pointer;
-
-    &:hover {
-      background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-      color: black;
-    }
-`;
 
 class App extends Component {
   state = {
@@ -102,11 +87,11 @@ class App extends Component {
       <div className="App">
         <h2>Hello, I'm a React App</h2>
         <p className={classes.join(' ')}>My name is Tu</p>
-        <StyledButton
-          alt={this.state.showPersons}
+        <button
+          className="button"
           onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
 
         {persons}
       </div>
